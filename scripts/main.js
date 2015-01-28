a0ce33488277d192eb169d8d77f5e47e339b4a71
Stephen Settle <ssettle3@gmail.com>
2015-01-27 23:15:42 -0500

    Add octicons pages to styles

--- scripts/main.js
+++ scripts/main.js
@@ -1181,101 +1181,88 @@ var org =
 ]
 var star = 
 []
-<<<<<<< HEAD
-// ================== Header User SECTION ========================
+
+
+// ==== Header User SECTION ====
 
 var usrSm, usrSmString, usrSmFunct, usrSmHTML;
 
-usrSm = $('.userImgSm');
-
-usrSmString = $('#userSmTemp').html();
-
-usrSmFunct = _.template(usrSmString);
-
-usrSmHTML = usrSmFunct;
-
-usrSm.append(usrSmHTML(user))
+	usrSm = $('.userImgSm');
+	usrSmString = $('#userSmTemp').html();
+	usrSmFunct = _.template(usrSmString);
+	usrSmHTML = usrSmFunct;
+	usrSm.append(usrSmHTML(user))
 
 
-=======
->>>>>>> master
 
-// ================== User SECTION =============================
+// ==== User SECTION ====
 
 
 var usr, templateString, templateFunct, templateHTML;
 
 	usr = $('.user')
-
 	templateString = $('#userTemp').html();
-
 	templateFunct = _.template(templateString);
-
 	templateHTML = templateFunct;
-
 	usr.append(templateHTML(user))
 
 
-// ================== Organization SECTION ===========================
-<<<<<<< HEAD
+// ==== Organization SECTION ====
 
 var orgTIY, orgString, orgFunct, orgHTML;
 
 	orgTIY = $('.orgs')
-
 	orgString = $('#orgTemp').html();
-=======
-/*
-var org, orgString, orgFunct, orgHTML;
-
-	org = $('.orgs')
-
-	orgString = $('orgTemp').html();
->>>>>>> master
-
 	orgFunct = _.template(orgString);
-
 	orgHTML = orgFunct;
-
-<<<<<<< HEAD
 	_.each(org, function(orgs){
 		orgTIY.append(orgHTML(orgs))
 	});
 
-=======
-	org.append(orgHTML(orgs))
 
-*/
->>>>>>> master
-
-// ================== Repository SECTION =============================
+// ==== Repository SECTION ====
 
 
-<<<<<<< HEAD
-var repo, repoString, repoFunct, repoHTML, time, dateTime;
-=======
-var repo, repoString, repoFunct, repoHTML;
->>>>>>> master
+var repo, sortRepos, filtRepo, repoString, repoFunct, repoHTML, time, dateTime;
 
-	repo = $('.repos')
 
-	repoString = $('#repoTemp').html();
-
-	repoFunct = _.template(repoString);
-
-	repoHTML = repoFunct;
-
-	_.each(repos, function(repos){
-		repo.append(repoHTML(repos))
+	// Sort Array to list repos in order of last updated
+	sortRepos = repos.sort(function (a, b){
+		if (a.updated_at < b.updated_at){
+			return 1;
+		}
+		if (a.updated_at > b.updated_at){
+			return -1;
+		}
 	});
 
-<<<<<<< HEAD
-/*
-	time = $('.time')
-	dateTime = 
 
-	time.append()
+	repo = $('.repos');
+	repoString = $('#repoTemp').html();
+	repoFunct = _.template(repoString);
+	repoHTML = repoFunct;
+	_.each(repos, function(sortRepos){
+		repo.append(repoHTML(sortRepos))
+	});
 
-*/
-=======
->>>>>>> master
+
+
+// ==== Date Stuff ====
+
+var date, dt, dtt, dttt, year, month, day, hours, minutes, seconds;
+	
+	joined = $('.join');
+
+	date = user.created_at;
+	dt = date.split("-");
+	year = dt[0];
+	month = dt[1];
+	dtt = dt[2].split("T");
+	day = dtt[0];
+	dttt = dtt[1].split(":");
+	hours = dttt[0];
+	minutes = dttt[1];
+
+	joined.append("Joined on " + month + " " + day +", " + year);
+
+
