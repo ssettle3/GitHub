85822e84f375c433b0b3335ac49c074129a93b69
Stephen Settle <ssettle3@gmail.com>
2015-01-27 20:45:53 -0500

    HTML, CSS, JS Clean up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,53 +1,41 @@
-// ================== Header User SECTION ========================
+// ==== Header User SECTION ====
 
 var usrSm, usrSmString, usrSmFunct, usrSmHTML;
 
-usrSm = $('.userImgSm');
-
-usrSmString = $('#userSmTemp').html();
-
-usrSmFunct = _.template(usrSmString);
-
-usrSmHTML = usrSmFunct;
-
-usrSm.append(usrSmHTML(user))
+	usrSm = $('.userImgSm');
+	usrSmString = $('#userSmTemp').html();
+	usrSmFunct = _.template(usrSmString);
+	usrSmHTML = usrSmFunct;
+	usrSm.append(usrSmHTML(user))
 
 
 
-// ================== User SECTION =============================
+// ==== User SECTION ====
 
 
 var usr, templateString, templateFunct, templateHTML;
 
 	usr = $('.user')
-
 	templateString = $('#userTemp').html();
-
 	templateFunct = _.template(templateString);
-
 	templateHTML = templateFunct;
-
 	usr.append(templateHTML(user))
 
 
-// ================== Organization SECTION ===========================
+// ==== Organization SECTION ====
 
 var orgTIY, orgString, orgFunct, orgHTML;
 
 	orgTIY = $('.orgs')
-
 	orgString = $('#orgTemp').html();
-
 	orgFunct = _.template(orgString);
-
 	orgHTML = orgFunct;
-
 	_.each(org, function(orgs){
 		orgTIY.append(orgHTML(orgs))
 	});
 
 
-// ================== Repository SECTION =============================
+// ==== Repository SECTION ====
 
 
 var repo, sortRepos, filtRepo, repoString, repoFunct, repoHTML, time, dateTime;
@@ -65,21 +53,11 @@ var repo, sortRepos, filtRepo, repoString, repoFunct, repoHTML, time, dateTime;
 
 
 	repo = $('.repos');
-
 	repoString = $('#repoTemp').html();
-
 	repoFunct = _.template(repoString);
-
 	repoHTML = repoFunct;
-
 	_.each(repos, function(sortRepos){
 		repo.append(repoHTML(sortRepos))
 	});
 
-/*
-	time = $('.time')
-	dateTime = 
 
-	time.append()
-
-*/
